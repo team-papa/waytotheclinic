bc31eb30afe92ebf8ef64e2ce3d5ca28a6c2a91d
Cjen1 <cjjensen01@aol.com>
2018-02-01 14:47:01 +0000

    Initial Commit
    Should contain everything required to build for android

---
+++ app/src/main/java/uk/ac/cam/cl/waytoclinic/waytotheclinic/LandingPage.kt
@@ -0,0 +1,12 @@
+package uk.ac.cam.cl.waytoclinic.waytotheclinic
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LandingPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_page)
+    }
+}
