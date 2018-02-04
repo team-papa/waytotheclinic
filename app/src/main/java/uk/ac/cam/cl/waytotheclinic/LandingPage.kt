8730e1bb975bc943878627fbecf3ba3449af3ae5
iunodora <tiz20@cam.ac.uk>
2018-02-04 22:27:01 +0000

    Implemented top bar (the one containing the search box) transition.

--- app/src/main/java/uk/ac/cam/cl/waytotheclinic/LandingPage.kt
+++ app/src/main/java/uk/ac/cam/cl/waytotheclinic/LandingPage.kt
@@ -1,12 +1,18 @@
 package uk.ac.cam.cl.waytotheclinic
 
+import android.animation.LayoutTransition
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.TypedValue
+import android.view.MotionEvent
 import android.view.View
 import android.widget.AutoCompleteTextView
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_landing_page.*
+import android.widget.Toast
+
 
 
 
@@ -38,7 +44,46 @@ class LandingPage : AppCompatActivity() {
                 top_green_box.setLayoutParams(params)
             }
         })
-    }
 
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            top_green_box.getLayoutTransition()
+                    .enableTransitionType(LayoutTransition.CHANGING);
+        }
+
+        var y1: Float = 0.0F
+        var y2: Float = 0.0F
+        var minSwipeDist: Float = 50.0F
+
+        val swipeListener = View.OnTouchListener { view, motionEvent ->
+            when (motionEvent.getAction()) {
+
+                MotionEvent.ACTION_DOWN -> {
+                    y1 = motionEvent.getY()
+                    true
+                }
+                MotionEvent.ACTION_UP -> {
+                    y2 = motionEvent.getY()
+
+                    if (y2 - y1 > minSwipeDist) {
+                        Toast.makeText(this@LandingPage, "Down swipe", Toast.LENGTH_SHORT).show()
+                        val params = top_green_box.getLayoutParams()
+                        params.height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300.0F, getResources().displayMetrics).toInt()
+                        top_green_box.setLayoutParams(params)
+                    } else if (y1 - y2 > minSwipeDist) {
+                        Toast.makeText(this@LandingPage, "Up swipe", Toast.LENGTH_SHORT).show()
+                        val params = top_green_box.getLayoutParams()
+                        params.height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 60.0F, getResources().displayMetrics).toInt()
+                        top_green_box.setLayoutParams(params)
+                    }
+                    true
+                }
+            }
+            true
+        }
+
+        top_green_box.setOnTouchListener(swipeListener);
+        menu_button.setOnTouchListener(swipeListener);
+    }
 
 }
