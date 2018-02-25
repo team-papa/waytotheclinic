b79144420100f002c9e1f647f26c4384c3f02acc
Unknown <cjjensen01@aol.com>
2018-02-25 16:48:01 +0000

    Fix git issues

--- app/src/main/java/uk/ac/cam/cl/waytotheclinic/MapFragment.java
+++ app/src/main/java/uk/ac/cam/cl/waytotheclinic/MapFragment.java
@@ -1,16 +1,217 @@
 package uk.ac.cam.cl.waytotheclinic;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class MapFragment extends Fragment {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.android.gms.maps.model.TileProvider;
+import com.google.android.gms.maps.model.UrlTileProvider;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import static android.content.ContentValues.TAG;
+
+public class MapFragment extends android.support.v4.app.Fragment implements OnMapReadyCallback{
+    MapView mapView = null;
+    GoogleMap googleMap;
+    public TileProvider mapTileProvider;
+    public TileOverlay mapOverlay;
+    public TileProvider pathTileProvider;
+    public String[] populatedTiles;
+    public int Floor;
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.map_view, container, true);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.map_view, container, false);
+
+        Thread urlLoader = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                URL url = null;
+                try {
+                    url = new URL("http://cjj39.user.srcf.net/WayToTheClinic/populatedTiles.txt");
+                    Scanner s = new Scanner(new BufferedInputStream(url.openStream()));
+                    String input = s.nextLine();
+                    String[] splitInput = input.split(";");
+                    populatedTiles = Arrays.copyOfRange(splitInput, 0, splitInput.length - 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        urlLoader.start();
+
+        return view;
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+
+        mapTileProvider = new UrlTileProvider(256,256) {
+            @Override
+            public URL getTileUrl(int x, int y, int zoom) {
+                Log.d(TAG, "getTileUrl: getting " + x + ", " + y + ", " + zoom);
+                try {
+                    if(!tilePopulated(Floor, zoom, x, y))
+                        return new URL("http://cjj39.user.srcf.net/WayToTheClinic/blank.png");
+                    String url = String.format("http://cjj39.user.srcf.net/WayToTheClinic/TileMap%d/%d/%d/%d.png", Floor, zoom, x, y);
+                    return new URL(url);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        };
+
+    }
+
+    private boolean tilePopulated(int f, int z, int x, int y){
+        String check = String.format("(%d,%d,%d,%d)", f,z,x,y);
+        for(String s : populatedTiles){
+            if(s.equals(check))
+                return true;
+        }
+        return false;
+    }
+
+//    public static void createPathFromString(Bitmap map, List<Pair> coords){
+//        Canvas canvas = new Canvas(map);
+//        canvas.drawColor(Color.TRANSPARENT);
+//        Paint paint = new Paint();
+//        paint.setARGB(255, 255, 0, 0);
+//        double scaling = 1.868;
+//        for(Pair p : coords){
+////            canvas.drawPoint((int)Math.floor(p.a * scaling), (int)Math.floor(p.b * scaling), paint);
+//            int radius = 2;
+//            int x = (int)Math.floor(p.a * scaling);
+//            int y = (int)Math.floor(p.b * scaling);
+//            canvas.drawCircle(x, y, radius, paint);
+//        }
+//        map.prepareToDraw();
+//    }
+
+    //region android boilerplate to get mapView working
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+        if(mapView == null)
+            mapView = getActivity().findViewById(R.id.mapView);
+
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+
+        mapView.onCreate(mapViewBundle);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if(mapView == null)
+            mapView = getActivity().findViewById(R.id.mapView);
+
+        mapView.getMapAsync(this);
+        mapView.setBackgroundColor(Color.BLACK);
+
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(mapView == null)
+            mapView = getActivity().findViewById(R.id.mapView);
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(mapView == null)
+            mapView = getActivity().findViewById(R.id.mapView);
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if(mapView == null)
+            mapView = getActivity().findViewById(R.id.mapView);
+        mapView.onStop();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        if(mapView == null)
+            mapView = getActivity().findViewById(R.id.mapView);
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(mapView == null)
+            mapView = getActivity().findViewById(R.id.mapView);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory(){
+        super.onLowMemory();
+        if(mapView == null)
+            mapView = getActivity().findViewById(R.id.mapView);
+        mapView.onLowMemory();
+    }
+
+    //endregion
 }
