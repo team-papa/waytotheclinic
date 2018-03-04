a172fb0586bd1c554f4f5919f3037d05755c623d
mattallsop <mattallsop5@gmail.com>
2018-03-04 18:32:44 +0000

    Change to new .ser

--- app/src/main/java/uk/ac/cam/cl/waytotheclinic/LocationsProvider.java
+++ app/src/main/java/uk/ac/cam/cl/waytotheclinic/LocationsProvider.java
@@ -15,7 +15,7 @@ public class LocationsProvider {
     public static Set<Vertex> generateVertices(Context context) {
         try {
             ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(
-                    context.getResources().getAssets().open("vertexSetSave.ser")));
+                    context.getResources().getAssets().open("vertexSetFinal.ser")));
 
             HashSet<Vertex> vertexSet = (HashSet<Vertex>) ois.readObject();
 
